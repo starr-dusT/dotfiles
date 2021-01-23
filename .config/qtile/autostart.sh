d4cdc063d7f7b4e60138892021ecb6a116fb09c6
starr-dusT <starrtyler88@gmail.com>
2021-01-22 17:42:11 -0800

    commit before qtile branch

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-nitrogen --restore &
+wal -i ~/media/pictures/wallpapers/random_wallpapers/sierra_red_mountains.jpg
 lxsession &
 xsetroot -cursor_name left_ptr
 imwheel -b 45
