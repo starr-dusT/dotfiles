3ba0f3c1fa1469d68a92795d5cebcb7834ad5265
Tyler Starr <starrtyler88@gmail.com>
2023-04-30 20:47:19 -0700

    remove submodules and add to main project

---
+++ provision/fedora/checklist.md
@@ -0,0 +1,18 @@
+# Checklist
+
+I can be forgetful so this document tracks what [additional setup](./additional-setup.md)
+has been done :)
+
+- [ ] Snapper
+- [ ] Wireguard Client
+- [ ] Mount network drives
+- [ X ] Taskopen for taskwarrior
+- [ ] Syncthing 
+- [ ] Lxappearance
+- [ ] Git SSH for personal and work
+- [ X ] Firefox
+- [ X ] Dracula colorscheme for gnome terminal
+- [ X ] Bluetooth Audio
+- [ X ] Linux-tkg kernel
+- [ X ] Single GPU Passthrough to windows
+- [ ] Google earth pro
---
+++ provision/fedora/jumpstart.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+echo -e "Starting initial setup for Fedora..."
+
+CHEZDIR="$HOME/.local/share/chezmoi"
+echo "Input email for bitwarden:"
+read bitemail
+
+# Install ansible python dependencies
+sudo dnf update -y
+sudo dnf install python3 python3-pip ansible cargo -y
+pip install pexpect
+cargo install rbw
+
+# Add things to path for this script
+export PATH="$PATH:/usr/local/bin"
+export PATH="$PATH:$HOME/.cargo/bin"
+export PATH="$PATH:$CHEZDIR/temp_bin"
+
+# Install ansible extensions
+ansible-galaxy install -r "$CHEZDIR/provision/fedora/ansible/requirements.yml"
+
+# Run setup playbook
+ansible-playbook "$CHEZDIR/provision/fedora/ansible/setup.yml" -i "$CHEZDIR/provision/fedora/ansible/hosts" --ask-become-pass
+
+# Copy jumpstart scripts to temp bin dir and add to path
+mkdir -p "$CHEZDIR/temp_bin"
+cp "$CHEZDIR/home/bin/executable_rbw-get" "$CHEZDIR/temp_bin/rbw-get"
+chmod +x "$CHEZDIR/temp_bin/rbw-get"
+
+# Set bitwarden email
+rbw config set email "$bitemail"
+
+# Make temporary i3 gen file
+mkdir -p "$CHEZDIR/home/.gen"
+echo "{
+    \"disp_pri\": \"HDMI-0\",
+    \"disp_sec\": \"HDMI-0\"
+}" > "$CHEZDIR/home/.gen/i3.json"
+
+# initialize chezmoi
+chezmoi init
+
+# first chezmoi apply
+chezmoi apply
+
+# reboot!
+read -p "You should reboot, but I won't make you. Reboot? " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+sudo reboot
