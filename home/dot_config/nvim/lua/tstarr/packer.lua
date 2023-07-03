8924341b4a97085adb01a592bef092caa61ba38d
Tyler Starr <starrtyler88@gmail.com>
2023-07-03 16:57:11 -0700

    update for debian

--- home/dot_config/nvim/lua/tstarr/packer.lua
+++ home/dot_config/nvim/lua/tstarr/packer.lua
@@ -46,6 +46,6 @@ return require('packer').startup(function(use)
 
     use('loctvl842/monokai-pro.nvim')
     require("monokai-pro").setup()
-    use('stevearc/oil.nvim')
     use('nvim-tree/nvim-tree.lua')
+    use('mcchrish/nnn.vim')
 end)
