cecef5e73a4c65a3b7bf946043da3443d624d219
Tyler Starr <starrtyler88@gmail.com>
2023-10-14 18:38:57 -0700

    Don't compile zk on iSH

--- provision/alpine/alpine-install-iSH.sh
+++ provision/alpine/alpine-install-iSH.sh
@@ -7,17 +7,13 @@ apk update
 
 # Install packages
 apk add --virtual .packs \
-    task timewarrior git go \
-    make fzf neovim nnn rbw \
+    task timewarrior git \
+    fzf neovim nnn rbw \
     chezmoi openssh bash
-
-# Build and install zk
-mkdir -p ~/tmp
-mkdir -p ~/bin
-git clone https://github.com/mickael-menu/zk.git ~/tmp/zk
-cd ~/tmp/zk 
-make
-mv zk ~/bin/zk
 
 # Clone dotfiles
 git clone https://github.com/starr-dusT/dotfiles.git ~/.local/share/chezmoi 
+
+# Move binaries from alpine pkgs
+mkdir -p ~/bin
+mv ~/.local/share/chezmoi/provision/alpine/pkgs/* ~/bin 
