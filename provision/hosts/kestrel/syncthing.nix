7ee5eb7cf49741bc2273999894725bdd3c7b946a
Tyler Starr <starrtyler88@gmail.com>
2023-11-09 16:12:34 -0800

    update syncthing for vault

--- provision/hosts/kestrel/syncthing.nix
+++ provision/hosts/kestrel/syncthing.nix
@@ -1,14 +1,20 @@
 { config, lib, pkgs, user, ... }:
 {
+  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+
   environment.systemPackages = with pkgs; [ syncthing ];
+
   services.syncthing = {
     enable = true;
     user = "${user}";
     configDir = "/home/${user}/.config/syncthing";
+    guiAddress = "0.0.0.0:8384";
     overrideDevices = true;
     overrideFolders = true; 
     devices = {
       "bulwark" = { id = "B5HZK2V-WA4WSQF-3JAIH4I-C6XQZ6J-EMMAIV5-CCYOA5G-N57GT6A-WH2GCQ2"; };
+      "torus" = { id = "WCZYHD7-5Y33SSU-74JHAQR-V7LYMDM-SDG2NTN-DJ2VKF2-DUBBUE5-PU5CGQN"; };
     };
     folders = {
       "Gamecube Saves" = {
@@ -18,6 +24,10 @@
       "Switch Saves" = {
         path = "/home/${user}/.local/share/yuzu/nand/user/save/0000000000000000/705C6CE0127692D598F92E68B640D644";
         devices = [ "bulwark" ];
+      };
+      "Vault" = {
+        path = "/home/${user}/documents/vault";
+        devices = [ "torus" ];
       };
     };
   };
