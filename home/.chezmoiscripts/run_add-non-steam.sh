69805610aa5153fed0a0f5b74a3259a25f83a3fe
Tyler Starr <starrtyler88@gmail.com>
2023-11-12 23:49:16 -0800

    add artwork and modify script for emulators in steam

--- home/.chezmoiscripts/run_add-non-steam.sh
+++ home/.chezmoiscripts/run_add-non-steam.sh
@@ -3,11 +3,12 @@
 
 steam_userid="47011563"
 shortcuts_vdf="$HOME/.steam/root/userdata/${steam_userid}/config/shortcuts.vdf"
+rm $shortcuts_vdf
 
 apps=(
-    " /run/current-system/sw/bin/dolphin-emu Dolphin"
-    "/run/current-system/sw/bin/yuzu Yuzu"
-    "/run/current-system/sw/bin/ppsspp PPSSPP"
+    "/run/current-system/sw/bin/dolphin-emu Dolphin Emulator"
+    "/run/current-system/sw/bin/yuzu Yuzu Emulator"
+    "/run/current-system/sw/bin/ppsspp PPSSPP Emulator"
 )
 
 if command -v steamtinkerlaunch &> /dev/null
@@ -15,8 +16,14 @@ then
     for app in "${apps[@]}"
     do
         set -- $app
-        if ! cat $shortcuts_vdf | grep -q "$2"; then
-            steamtinkerlaunch addnonsteamgame -ep="$1" -an="$2"
-        fi
+        steamtinkerlaunch addnonsteamgame \
+            -ep="$1" -an="$2" \
+            --iconpath="$HOME/.local/share/chezmoi/img/$2/icons.ico" \
+            --hero="$HOME/.local/share/chezmoi/img/$2/hero.png" \
+            --logo="$HOME/.local/share/chezmoi/img/$2/logo.png" \
+            --boxart="$HOME/.local/share/chezmoi/img/$2/boxart.png" \
+            --tenfoot="$HOME/.local/share/chezmoi/img/$2/tenfoot.png" \
+            --tags="$3" \
+            --copy
     done
 fi
