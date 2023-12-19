52ee6a1c0dd6d16a06b067ee71568dff5a3631c3
Tyler Starr <starrtyler88@gmail.com>
2023-12-19 00:23:52 -0800

    remove undotree

--- home/dot_config/nvim/after/plugin/keymap/undotree.lua
+++
@@ -1,9 +0,0 @@
-local wk = require("which-key")
-
-wk.register({
-    q = {
-        name = "+undotree",
-        u = { "<cmd>UndotreeFocus<CR>", "Focus Tree" },
-        t = { "<cmd>UndotreeToggle<CR>", "Toggle Tree" }
-    },
-}, { prefix = "<leader>" })
