4766d8b16b92127266397cd12b0dda24a27ab7d4
Tyler Starr <starrtyler88@gmail.com>
2023-12-28 12:54:21 -0800

    allow to notify current sink

--- home/dot_config/hypr/scripts/executable_audio-device-switcher.sh
+++ home/dot_config/hypr/scripts/executable_audio-device-switcher.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 #
-sink=$(pactl list sinks | grep "Sink #" | head -n $1 | tail -n 1)
-card=$(pactl list sinks | grep "alsa.card_name" | head -n $1 | tail -n 1)
-pactl set-default-sink $(echo $sink | cut -d "#" -f 2)
-notify-send "$(echo $card | cut -d "\"" -f 2)"
+if [[ $1 -eq 0 ]]; then
+   notify-send "$(pactl list sinks | awk -F': ' '/State:.*RUNNING/{getline; getline; sub("^[ \t]+", "", $2); print "Active Sink: " $2}')"
+else
+    sink=$(pactl list sinks | grep "Sink #" | head -n $1 | tail -n 1)
+    card=$(pactl list sinks | grep "alsa.card_name" | head -n $1 | tail -n 1)
+    pactl set-default-sink $(echo $sink | cut -d "#" -f 2)
+    notify-send "Active Sink: $(echo $card | cut -d "\"" -f 2)"
+fi
