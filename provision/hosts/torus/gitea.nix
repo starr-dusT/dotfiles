f40ded725eee68bb21854ec007cbac10de0c4f65
Tyler Starr <starrtyler88@gmail.com>
2024-05-26 22:16:00 -0700

    first pass at runner for torus

--- provision/hosts/torus/gitea.nix
+++ provision/hosts/torus/gitea.nix
@@ -15,8 +15,39 @@
       HTTP_PORT = 3001;
       ROOT_URL = "https://git.tstarr.us";
     };
-    #settings.actions = {
-    #  ENABLED = true;
-    #};
   };
+
+  sops.secrets = {
+    "tokens/gitea-runner" = {
+      sopsFile = ../../secrets/secrets.yaml;
+      owner = "gitea-runner";
+    };
+  };
+  services.gitea-actions-runner.instances = {
+    native-runner = {
+      enable = true;
+      url = "https://git.tstarr.us";
+      tokenFile = config.sops.secrets.gitea-runner.path;
+      name = "native-runner";
+      labels = [
+        "native:host"
+      ];
+      hostPackages = with pkgs; [
+        bash
+        coreutils
+        curl
+        gawk
+        gitMinimal
+        gnused
+        nodejs-18_x
+        wget
+      ];
+    };
+  };
+  users.users.gitea-runner = {
+    createHome = false;
+    isSystemUser = true;
+    group = "gitea-runner";
+  };
+  users.groups.gitea-runner = {};
 }
