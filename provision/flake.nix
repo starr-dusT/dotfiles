93c349d5bdbb139b52ce818129a09536060a7930
Tyler Starr <starrtyler88@gmail.com>
2024-07-21 16:53:06 -0700

    refactor flake for stupid reasons

--- provision/flake.nix
+++ provision/flake.nix
@@ -1,8 +1,9 @@
 {
-  description = "Flake for nixos configurations";
+  description = "Flake to manage my nixos machines";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    agenix.url = "github:ryantm/agenix";
     home-manager = {
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,7 +12,6 @@
       url = "git+https://github.com/Jovian-Experiments/Jovian-NixOS?ref=development";
       flake = false;
     };
-    agenix.url = "github:ryantm/agenix";
     hyprland.url = "github:hyprwm/Hyprland";
     hyprland-contrib = {
         url = "github:hyprwm/contrib";
@@ -20,102 +20,102 @@
   };
 
   outputs = inputs @ { self, nixpkgs, home-manager, jovian-nixos, agenix, hyprland, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
+  let
+    system = "x86_64-linux";
+    user = "tstarr";
+    lib = nixpkgs.lib;
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    nixosConfigurations = {
+      kestrel = lib.nixosSystem {
         inherit system;
+        specialArgs = { inherit user; inherit inputs; };
+        modules = [
+          ./modules
+          ./hosts/kestrel/configuration.nix
+          ./hosts/kestrel/hardware.nix
+          agenix.nixosModules.default
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit user; };
+            home-manager.users.${user} = {
+              imports = [ 
+                ./home-modules
+                ./hosts/kestrel/home-configuration.nix 
+              ];
+            };
+          }
+        ];
       };
-      lib = nixpkgs.lib;
-      user = "tstarr";
-    in {
-      nixosConfigurations = {
-        kestrel = lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit user; inherit inputs; };
-          modules = [
-            ./modules
-            ./hosts/kestrel/configuration.nix
-            ./hosts/kestrel/hardware.nix
-            agenix.nixosModules.default
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit user; };
-              home-manager.users.${user} = {
-                imports = [ 
-                  ./home-modules
-                  ./hosts/kestrel/home-configuration.nix 
-                ];
-              };
-            }
-          ];
-        };
 
-        shivan = lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit user; inherit inputs; };
-          modules = [
-            ./modules
-            ./hosts/shivan/configuration.nix
-            ./hosts/shivan/hardware.nix
-            agenix.nixosModules.default
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit user; };
-              home-manager.users.${user} = {
-                imports = [ 
-                  ./home-modules
-                  ./hosts/shivan/home-configuration.nix 
-                ];
-              };
-            }
-          ];
-        };
+      shivan = lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit user; inherit inputs; };
+        modules = [
+          ./modules
+          ./hosts/shivan/configuration.nix
+          ./hosts/shivan/hardware.nix
+          agenix.nixosModules.default
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit user; };
+            home-manager.users.${user} = {
+              imports = [ 
+                ./home-modules
+                ./hosts/shivan/home-configuration.nix 
+              ];
+            };
+          }
+        ];
+      };
 
-        torus = lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit user; inherit inputs; };
-          modules = [
-            ./modules
-            ./hosts/torus/configuration.nix
-            ./hosts/torus/hardware.nix
-            agenix.nixosModules.default
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit user; };
-              home-manager.users.${user} = {
-                imports = [ 
-                  ./home-modules
-                  ./hosts/torus/home-configuration.nix 
-                ];
-              };
-            }
-          ];
-        };
+      torus = lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit user; inherit inputs; };
+        modules = [
+          ./modules
+          ./hosts/torus/configuration.nix
+          ./hosts/torus/hardware.nix
+          agenix.nixosModules.default
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit user; };
+            home-manager.users.${user} = {
+              imports = [ 
+                ./home-modules
+                ./hosts/torus/home-configuration.nix 
+              ];
+            };
+          }
+        ];
+      };
 
-        bulwark = lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit user; inherit inputs; inherit jovian-nixos; inherit home-manager; };
-          modules = [
-            ./modules
-            ./hosts/bulwark/configuration.nix
-            ./hosts/bulwark/hardware.nix
-            agenix.nixosModules.default
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit user; };
-              home-manager.users.${user} = {
-                imports = [ 
-                  ./home-modules
-                  ./hosts/bulwark/home-configuration.nix 
-                ];
-              };
-            }
-          ];
-        };
+      bulwark = lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit user; inherit inputs; inherit jovian-nixos; inherit home-manager; };
+        modules = [
+          ./modules
+          ./hosts/bulwark/configuration.nix
+          ./hosts/bulwark/hardware.nix
+          agenix.nixosModules.default
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit user; };
+            home-manager.users.${user} = {
+              imports = [ 
+                ./home-modules
+                ./hosts/bulwark/home-configuration.nix 
+              ];
+            };
+          }
+        ];
       };
     };
+  };
 }
