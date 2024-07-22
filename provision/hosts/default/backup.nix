8e0e5c948817d02708f0e4af8fd6adf579ee1d1a
Tyler Starr <starrtyler88@gmail.com>
2024-07-21 20:47:43 -0700

    passwordless login for kestrel restic backup

--- provision/hosts/default/backup.nix
+++ provision/hosts/default/backup.nix
@@ -1,5 +1,10 @@
 { config, pkgs, user, lib, ... }:
 {
+  # Password-less logins for backup
+  users.users."${user}".openssh.authorizedKeys.keyFiles = [
+    config.age.secrets."ssh/kestrel/id_ed25519.pub".path
+  ];
+
   environment.systemPackages = with pkgs; [
     restic # Fast and secure backup program
   ];
