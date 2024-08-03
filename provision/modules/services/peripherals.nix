63d91bf870268c8588b536a499ae38c9cc1138cf
Tyler Starr <starrtyler88@gmail.com>
2024-08-02 23:40:44 -0700

    allow usb connections with iPhones

--- provision/modules/services/peripherals.nix
+++ provision/modules/services/peripherals.nix
@@ -5,12 +5,14 @@ in {
   options.modules.services.peripherals.enable = lib.mkEnableOption "peripherals";
   config = lib.mkIf cfg.enable {
 
+    services.usbmuxd.enable = true; # for iOS mounting as storage
     environment.systemPackages = with pkgs; [
       pulseaudio # Sound server for Linux and other Unix-like operating systems.
       pavucontrol # Simple GTK based mixer for the PulseAudio sound server
+      libimobiledevice # Library to support iPhone, iPod Touch and iPad devices on Linux
+      ifuse # fuse filesystem implementation to access the contents of iOS devices
     ];
 
-    # rtkit is optional but recommended
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
