e12ed0f124bda6d916c74941d4f881ab83bfaa04
Tyler Starr <starrtyler88@gmail.com>
2024-09-13 19:54:30 -0700

    create backup store on kestrel

--- provision/hosts/kestrel/backup.nix
+++ provision/hosts/kestrel/backup.nix
@@ -4,5 +4,8 @@
   users.users."${user}".openssh.authorizedKeys.keyFiles = [
     config.age.secrets."ssh/torus/id_ed25519.pub".path
   ];
+  systemd.tmpfiles.rules = [
+    "d /store 0775 ${user} users -"
+  ];
 }
           
