1edff2811fe14d0ef53f97316655186f3292bf16
Tyler Starr <starrtyler88@gmail.com>
2024-09-30 10:40:53 -0700

    by default install git module

--- provision/modules/programs/git.nix
+++ provision/modules/programs/git.nix
@@ -1,15 +1,25 @@
-{ config, pkgs, user, lib, ... }:
-{
-  environment.systemPackages = with pkgs; [
-    git # Version control system for tracking changes in source code during software development.
-    git-annex # Manages files with git, without checking the file contents into git.
-    lazygit # Terminal-based GUI for git, making it easier to use and visualize git repositories.
-  ];
+{ config, lib, pkgs, user, ... }:
 
-  age.secrets."git/github_personal" = {
-    file = ../../secrets/git/github_personal.age;
-    owner = "${user}";
-    group = "users";
+let cfg = config.modules.programs.git;
+in {
+  options.modules.programs.git = with lib; {
+    enable = lib.mkOption {
+      type = with types; bool;
+      default = true;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      git # Version control system for tracking changes in source code during software development.
+      git-annex # Manages files with git, without checking the file contents into git.
+      lazygit # Terminal-based GUI for git, making it easier to use and visualize git repositories.
+    ];
+
+    age.secrets."git/github_personal" = {
+      file = ../../secrets/git/github_personal.age;
+      owner = "${user}";
+      group = "users";
+    };
   };
 }
-          
