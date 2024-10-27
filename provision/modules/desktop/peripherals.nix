b2a6bcd8edee3059518b17dee5bae8a66dc9ba8d
Tyler Starr <starrtyler88@gmail.com>
2024-10-26 19:41:01 -0700

    move CAC tools to peripherals

--- provision/modules/desktop/peripherals.nix
+++ provision/modules/desktop/peripherals.nix
@@ -4,11 +4,14 @@ let cfg = config.modules.desktop.peripherals;
 in {
   options.modules.desktop.peripherals.enable = lib.mkEnableOption "peripherals";
   config = lib.mkIf cfg.enable {
-    services.usbmuxd.enable = true; # for iOS mounting as storage
     environment.systemPackages = with pkgs; [
       libimobiledevice # Library to support iPhone, iPod Touch and iPad devices on Linux
       ifuse # fuse filesystem implementation to access the contents of iOS devices
       pySVS # control SVS subwoofers from the command-line
+      opensc # Open source smart card tools and middleware
+      pcsc-tools # Tools are used to test a PC/SC drivers
+      pkcs11helper # Library that simplifies the interaction with PKCS#11
     ];
+    services.usbmuxd.enable = true; # for iOS mounting as storage
   };
 }
