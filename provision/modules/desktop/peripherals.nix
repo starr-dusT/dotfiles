3b1c85cec71a570c7211709899ef01a965e4db46
Tyler Starr <starrtyler88@gmail.com>
2024-11-07 20:58:34 -0800

    move sv command to scriptBin for gnome run dialog

--- provision/modules/desktop/peripherals.nix
+++ provision/modules/desktop/peripherals.nix
@@ -7,6 +7,10 @@ in {
       libimobiledevice # Library to support iPhone, iPod Touch and iPad devices on Linux
       ifuse # fuse filesystem implementation to access the contents of iOS devices
       pySVS # control SVS subwoofers from the command-line
+      (pkgs.writeScriptBin "sv" ''
+        #!/bin/sh
+        pySVS 54:B7:E5:57:1A:7B --volume="$1"
+      '')
       opensc # Open source smart card tools and middleware
       pcsc-tools # Tools are used to test a PC/SC drivers
       pkcs11helper # Library that simplifies the interaction with PKCS#11
