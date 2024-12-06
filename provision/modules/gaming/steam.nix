3f4084fa3038db4c707d111503c95e3bab78b128
Tyler Starr <starrtyler88@gmail.com>
2024-12-05 21:50:06 -0800

    add heroic game launcher

--- provision/modules/gaming/steam.nix
+++ provision/modules/gaming/steam.nix
@@ -8,6 +8,7 @@ in {
       gamescope # Utility for running games using Valve's Steam Play compatibility layer with improved performance and compatibility
       mangohud # Vulkan and OpenGL overlay for monitoring FPS, temperatures, CPU/GPU load and more
       steamtinkerlaunch # Launcher and optimization tool for Steam games
+      heroic # Native GOG Launcher
     ];
 
     hardware.graphics.enable = true; # https://github.com/NixOS/nixpkgs/issues/47932
