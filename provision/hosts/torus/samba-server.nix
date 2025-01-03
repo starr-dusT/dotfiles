f09ab8fab6d1c6b77d7f6a385f00be115b032e48
Tyler Starr <starrtyler88@gmail.com>
2025-01-03 00:16:49 -0800

    update samba config for newer nixpkgs

--- provision/hosts/torus/samba-server.nix
+++ provision/hosts/torus/samba-server.nix
@@ -2,16 +2,18 @@
 {
   services.samba = {
     enable = true;
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server string = smbnix
-      netbios name = smbnix
-      security = user 
-      hosts allow = 192.168.3. 192.168.1. 127.0.0.1 localhost
-      hosts deny = 0.0.0.0/0
-      guest account = nobody
-      map to guest = bad user
-    '';
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        "hosts allow" = [ "192.168.3." "192.168.1." "127.0.0.1" "localhost" ];
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+    };
 
     shares = {
       private = {
