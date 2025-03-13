b50cd6b3245ad1ea9034ebe9ab803eee1408c550
Tyler Starr <starrtyler88@gmail.com>
2025-03-12 22:28:27 -0700

    reorder base terminal

--- provision/modules/base/terminal.nix
+++ provision/modules/base/terminal.nix
@@ -1,22 +1,24 @@
 { config, lib, pkgs, user, ... }:
 {
   environment.systemPackages = with pkgs; [
-    killall # Command-line utility to terminate processes by name
-    pciutils # Utilities for inspecting and manipulating devices connected to the PCI bus
-    btop # Terminal-based resource monitor, providing an interactive view of system resources
-    nix-search-cli # Command-line utility for searching the Nix package repository
-    rbw # Command-line interface to the Bitwarden password manager
-    pinentry-curses # Simple curses-based passphrase entry dialog for GnuPG
     bash # GNU Bourne-Again SHell, a command language interpreter for Unix-like operating systems
     bash-completion # Provides programmable completion for the bash shell
     tmux # Terminal multiplexer, allowing multiple terminal sessions within a single window
-    nnn # Terminal file manager with a focus on performance and ease of use
-    advcpmv # Advanced version of the Unix utilities cp and mv
+    killall # Command-line utility to terminate processes by name
+    pciutils # Utilities for inspecting and manipulating devices connected to the PCI bus
+    pinentry-curses # Simple curses-based passphrase entry dialog for GnuPG
     unzip # Command-line utility for extracting files from ZIP archives
-    trash-cli # Command-line interface to the freedesktop.org Trash
     vim # Text editor that is highly configurable and widely used, especially in the Unix environment
+
+    rbw # Command-line interface to the Bitwarden password manager
+    advcpmv # Advanced version of the Unix utilities cp and mv
+    trash-cli # Command-line interface to the freedesktop.org Trash
     ripgrep # Line-oriented search tool that recursively searches directories for a regex pattern
     sesh # Smart session manager for the terminal
     fzf # Command-line fuzzy finder for Unix-like operating systems
+    nix-search-cli # Command-line utility for searching the Nix package repository
+
+    btop # Terminal-based resource monitor, providing an interactive view of system resources
+    nnn # Terminal file manager with a focus on performance and ease of use
   ];
 }
