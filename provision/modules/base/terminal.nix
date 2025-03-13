e9e182eebb6f7468b8c24b5a6f23f44bf7be7ed9
Tyler Starr <starrtyler88@gmail.com>
2025-03-12 23:43:25 -0700

    add zoxide back for sesh

--- provision/modules/base/terminal.nix
+++ provision/modules/base/terminal.nix
@@ -11,6 +11,7 @@
     vim # Text editor that is highly configurable and widely used, especially in the Unix environment
 
     sesh # Smart session manager for the terminal
+    zoxide # Fast cd command that learns your habits
     ripgrep # Line-oriented search tool that recursively searches directories for a regex pattern
     fzf # Command-line fuzzy finder for Unix-like operating systems
     nix-search-cli # Command-line utility for searching the Nix package repository
