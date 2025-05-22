97f0bc5b3c8faac53cfde70b4291386f5a878c06
Tyler Starr <starrtyler88@gmail.com>
2025-05-21 21:58:46 -0700

    install prusaslicer with flatpak

--- provision/modules/base/plus/default.nix
+++ provision/modules/base/plus/default.nix
@@ -1,9 +1,14 @@
-{ lib, ... }:
-{
+{ lib, config, ... }:
+
+let cfg = config.modules.base-plus;
+in {
   imports = [
     ../../programs/beancount.nix
     ../../programs/borg.nix
     ../../programs/yt-dlp.nix
   ];
   options.modules.base-plus.enable = lib.mkEnableOption "base-plus";
+  config = lib.mkIf cfg.enable {
+    services.flatpak.enable = true;
+  };
 }
