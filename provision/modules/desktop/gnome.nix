796234dab572c242b8b56a9ce4d82a977ef809ae
Tyler Starr <starrtyler88@gmail.com>
2025-08-25 22:51:59 -0700

    enable rdp for gnome

--- provision/modules/desktop/gnome.nix
+++ provision/modules/desktop/gnome.nix
@@ -63,6 +63,22 @@ in {
     services.pulseaudio.enable = lib.mkForce false;
     services.gvfs.enable = true;
 
+     # Enable the GNOME RDP components
+    services.gnome.gnome-remote-desktop.enable = true;
+    
+    # Ensure the service starts automatically at boot so the settings panel appears
+    systemd.services.gnome-remote-desktop = {
+      wantedBy = [ "graphical.target" ];
+    };
+    
+    # Open the default RDP port (3389)
+    networking.firewall.allowedTCPPorts = [ 3389 ];
+    
+    # Disable autologin to avoid session conflicts
+    services.displayManager.autoLogin.enable = false;
+    services.getty.autologinUser = null;
+
+
     xdg.mime = {
       enable = true;
       defaultApplications = {    
