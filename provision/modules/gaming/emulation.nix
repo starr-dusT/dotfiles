721c32ad1b7e92152e2f3665450bfcee00e78c8a
Tyler Starr <starrtyler88@gmail.com>
2025-09-01 17:24:36 -0700

    Use retrodeck for emulation

--- provision/modules/gaming/emulation.nix
+++ provision/modules/gaming/emulation.nix
@@ -1,25 +1,12 @@
 { config, lib, pkgs, user, ... }:
 
 let cfg = config.modules.gaming.emulation;
-    retroarchWithCores = (pkgs.retroarch.withCores (cores: with cores; [
-      nestopia # Nintendo Entertainment System (NES) emulator
-      snes9x # Super Nintendo Entertainment System (SNES) emulator
-      mgba # Game Boy Advance emulator
-      melonds # Nintendo DS emulator
-      citra # Fuck Nintendo a third time
-    ]));
 in {
   options.modules.gaming.emulation.enable = lib.mkEnableOption "emulation";
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ 
-      ryubing
-      dolphin-emu # GameCube and Wii emulator
-      ppsspp # PSP emulator
-      mgba # Game Boy Advance emulator
-      rpcs3 # PS3 emulator/debugger
-      retroarchWithCores # frontend for emulators and specific cores
-      openmw # Open-source open-world RPG game engine that supports playing Morrowind
+    services.flatpak.packages = [
+      "net.retrodeck.retrodeck"
     ];
   };
 }
