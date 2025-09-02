65023f9b9beb667b50bb2e8ff0fbe33bb12e1b7b
Tyler Starr <starrtyler88@gmail.com>
2025-09-01 17:38:16 -0700

    remove uneeded import

--- provision/modules/base/plus.nix
+++ provision/modules/base/plus.nix
@@ -2,12 +2,6 @@
 
 let cfg = config.modules.base-plus;
 in {
-  imports = [
-    ../programs/beancount.nix
-    ../programs/borg.nix
-    ../programs/yt-dlp.nix
-  ];
-
   options.modules.base-plus.enable = lib.mkEnableOption "base-plus";
 
   config = {
