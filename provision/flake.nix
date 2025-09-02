e140ce645f6e60d02177428b4fabfc0496c52ee6
Tyler Starr <starrtyler88@gmail.com>
2025-09-01 17:09:01 -0700

    remove wsl

--- provision/flake.nix
+++ provision/flake.nix
@@ -6,12 +6,10 @@
     agenix.url = "github:ryantm/agenix";
     home-manager.url = github:nix-community/home-manager;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL";
-    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.6.0";
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, agenix, nixos-wsl, nix-flatpak, ... }:
+  outputs = inputs @ { self, nixpkgs, home-manager, agenix, nix-flatpak, ... }:
   let
     system = "x86_64-linux";
     hosts = builtins.fromJSON (builtins.readFile ./hosts.json);
@@ -20,12 +18,13 @@
     nixosConfigurations = lib.mapAttrs (hostname: hostConfig:
       lib.nixosSystem ({
         specialArgs = {
-          inherit lib system inputs agenix home-manager nixos-wsl nix-flatpak;
+          inherit lib system inputs agenix home-manager nix-flatpak;
           user = hostConfig.user;
           hostname = "${hostname}";
         };
         modules = [
           ./hosts/${hostname}/configuration.nix
+          ./hosts/${hostname}/hardware.nix
           ./modules
           agenix.nixosModules.default
           nix-flatpak.nixosModules.nix-flatpak
@@ -34,8 +33,7 @@
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = { user = hostConfig.user; };
           }
-        ] ++ (if "${hostname}" == "wsl" then []
-              else [./hosts/${hostname}/hardware.nix]);
+        ];
       })
     ) hosts;
   };
--- provision/hosts/ariolimax/readme.md
+++
@@ -1,2 +0,0 @@
-# Ariolimax
-> WSL image
