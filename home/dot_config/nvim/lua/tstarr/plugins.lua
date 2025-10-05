157cdfa0587cb73516d18ae53373633c75f4b20d
Tyler Starr <starrtyler88@gmail.com>
2025-10-04 22:39:37 -0700

    remove lsp for now

--- home/dot_config/nvim/lua/tstarr/plugins.lua
+++ home/dot_config/nvim/lua/tstarr/plugins.lua
@@ -20,26 +20,6 @@ local plugins = {
     'nvim-lua/plenary.nvim',
     "mickael-menu/zk-nvim",
     "nvim-treesitter/nvim-treesitter-context",
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        dependencies = {
-      	  -- LSP Support
-      	  {'neovim/nvim-lspconfig'},
-      	  {'williamboman/mason.nvim'},
-      	  {'williamboman/mason-lspconfig.nvim'},
-      	  -- Autocompletion
-      	  {'hrsh7th/nvim-cmp'},
-      	  {'hrsh7th/cmp-buffer'},
-      	  {'hrsh7th/cmp-path'},
-      	  {'saadparwaiz1/cmp_luasnip'},
-      	  {'hrsh7th/cmp-nvim-lsp'},
-      	  {'hrsh7th/cmp-nvim-lua'},
-      	  -- Snippets
-      	  {'L3MON4D3/LuaSnip'},
-      	  {'rafamadriz/friendly-snippets'},
-        }
-    }
 }
 
 local opts = {}
