d5d2fd1159468cd7d43a882bc55b1c153fc8bc14
Tyler Starr <starrtyler88@gmail.com>
2025-10-20 15:55:06 -0700

    remove pyright

--- provision/modules/core/programs/nvim.nix
+++ provision/modules/core/programs/nvim.nix
@@ -2,6 +2,5 @@
 {
   environment.systemPackages = with pkgs; [
     neovim # Fork of Vim aiming to improve extensibility and usability
-    pyright # Latest version of the Pyright package, a static type checker for Python
   ];
 }
