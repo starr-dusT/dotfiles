b72119123c783a2f070794ca2b3e735493330c59
Tyler Starr <starrtyler88@gmail.com>
2025-10-20 00:17:43 -0700

    resolve warnings with flake.nix

--- provision/flake.nix
+++ provision/flake.nix
@@ -4,19 +4,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
-    home-manager.url = github:nix-community/home-manager;
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.6.0";
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, agenix, nix-flatpak, ... }:
+  outputs = inputs @ { nixpkgs, home-manager, agenix, nix-flatpak, ... }:
   let
     system = "x86_64-linux";
     hosts = builtins.fromJSON (builtins.readFile ./hosts.json);
     lib = nixpkgs.lib;
   in {
     nixosConfigurations = lib.mapAttrs (hostname: hostConfig:
-      lib.nixosSystem ({
+      lib.nixosSystem {
         specialArgs = {
           inherit lib system inputs agenix home-manager nix-flatpak;
           user = hostConfig.user;
@@ -34,7 +34,7 @@
             home-manager.extraSpecialArgs = { user = hostConfig.user; };
           }
         ];
-      })
+      }
     ) hosts;
   };
 }
