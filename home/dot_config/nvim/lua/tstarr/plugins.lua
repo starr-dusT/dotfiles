62d2c13f3bd34168e5f222f464bce0cbfd8ca52a
Tyler Starr <starrtyler88@gmail.com>
2025-10-21 12:40:38 -0700

    move nvim-lspconfig out of main plugins file

--- home/dot_config/nvim/lua/tstarr/plugins.lua
+++ home/dot_config/nvim/lua/tstarr/plugins.lua
@@ -15,57 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
     "ellisonleao/gruvbox.nvim",
     "norcalli/nvim-colorizer.lua",
-    {
-      "neovim/nvim-lspconfig",
-      lazy = false,
-      dependencies = {
-        { "ms-jpq/coq_nvim", branch = "coq" },
-        { "ms-jpq/coq.artifacts", branch = "artifacts" },
-        { 'ms-jpq/coq.thirdparty', branch = "3p" }
-      },
-      init = function()
-        vim.g.coq_settings = {
-            auto_start = "shut-up",
-        }
-      end,
-      config = function()
-        vim.lsp.enable('nixd')
-        vim.lsp.config('nixd', {
-          cmd = { "nixd" },
-          settings = {
-            nixd = {
-              nixpkgs = {
-                -- For flake.
-                -- This expression will be interpreted as "nixpkgs" toplevel
-                -- Nixd provides package, lib completion/information from it.
-                -- Resource Usage: Entries are lazily evaluated, entire nixpkgs takes 200~300MB for just "names".
-                -- Package documentation, versions, are evaluated by-need.
-                expr = "import (builtins.getFlake(toString ./.)).inputs.nixpkgs { }",
-              },
-              formatting = {
-                command = { "nixfmt" }, -- or nixfmt or nixpkgs-fmt
-              },
-              options = {
-                nixos = {
-                  expr = "let flake = builtins.getFlake(toString ./.); in flake.nixosConfigurations.nz.options",
-                },
-                home_manager = {
-                  expr = 'let flake = builtins.getFlake(toString ./.); in flake.homeConfigurations."sab@mbp16".options',
-                },
-                darwin = {
-                  expr = "let flake = builtins.getFlake(toString ./.); in flake.darwinConfigurations.mbp16.options",
-                },
-              },
-            },
-          },
-        })
-        vim.diagnostic.config({
-          virtual_lines = {
-            current_line = true,
-          },
-        })
-      end,
-    }
+    require("tstarr.lsp-config"),
 }
 
 local opts = {}
