0f5576616425291b207d8c1b9696431659e9ae31
Tyler Starr <starrtyler88@gmail.com>
2025-10-27 16:22:53 -0700

    update scripts to work with homeassistant

--- provision/modules/optional/desktop/scripts/display-switch.sh
+++ provision/modules/optional/desktop/scripts/display-switch.sh
@@ -6,8 +6,15 @@ function display_help() {
     echo "Only valid selections are: kestrel-living, kestrel-desktop"
 }
 
-while getopts ":hv" opt; do
+args=()
+val="$1"
+
+while getopts "p:h" opt; do
     case $opt in
+        p )
+            args=(--persistent)
+            val="$2"
+            ;;
         h )
             display_help
             exit 0
@@ -18,12 +25,12 @@ done
 # Get location of mutter in nix store to work around gdctl not being in PATH
 # https://github.com/NixOS/nixpkgs/issues/416824
 mutter=$(echo "$(nix eval nixpkgs#mutter.outPath)" | sed -r 's/\"//g')
-case $1 in
+case $val in
     "kestrel-living" )
-        "$mutter/bin/gdctl" set --persistent --logical-monitor --primary --monitor HDMI-1 --mode 2560x1440@59.951 --scale 1.5
+        "$mutter/bin/gdctl" set "${args[@]}" --logical-monitor --primary --monitor HDMI-1 --mode 2560x1440@59.951 --scale 1.5
         ;;
     "kestrel-desktop" )
-        "$mutter/bin/gdctl" set --persistent --logical-monitor --primary --monitor DP-2 --mode 2560x1440@143.912 --scale 1 --logical-monitor --monitor DP-1 --mode 2560x1440@143.973 --scale 1 --left-of DP-2;
+        "$mutter/bin/gdctl" set "${args[@]}" --logical-monitor --primary --monitor DP-2 --mode 2560x1440@143.912 --scale 1 --logical-monitor --monitor DP-1 --mode 2560x1440@143.973 --scale 1 --left-of DP-2
         ;;
     * )
         echo "Only valid selections are kestrel-living, kestrel-desktop"
