bef969c15f03b6ab5570f2ccdbb4938456e347c5
Tyler Starr <starrtyler88@gmail.com>
2025-10-29 21:03:38 -0700

    add connections to stormwalker

--- provision/hosts/stormwalker/configuration.nix
+++ provision/hosts/stormwalker/configuration.nix
@@ -27,6 +27,24 @@
         _plus.enable = true;
       };
       scripts.enable = true;
-    };
+      services = {                                                                                                                       
+        syncthing = {                                                                                                                    
+          enable = true;                                                                                                                 
+          keyPath = ../../secrets/syncthing/kestrel/key.pem.age;                                                                         
+          certPath = ../../secrets/syncthing/kestrel/cert.pem.age;                                                                       
+          devices = {                                                                                                                    
+            "kestrel" = {
+              id = "5WWL4FE-ARZ4FHP-J33HQCH-CZKEXLN-2RAY4KW-PDI754F-3HVPZYI-VC3ESAF";
+            };
+            "kruos" = {                                                                                                                  
+              id = "VROOKEG-H75SHUK-FK2LRAI-DU77ULS-WB6ZMQ2-7AIE5SQ-IDZLU7Q-Z762ZQU";                                                    
+            };                                                                                                                           
+            "torus" = {                                                                                                                  
+              id = "ZVABUCA-3SA5QKR-OZSCIS5-RDAHR2V-D4R4NFK-ZBYOKDP-6HQUG2M-BNL3DAO";                                                    
+            };                                                                                                                           
+          };                                                                                                                             
+        };                                                                                                                               
+      };                                                                                                                                 
+    };                                                                                                                                   
   };
 }
