0d2bba4396f8a2c4ab9124b7493c2e00c9e918ed
Tyler Starr <starrtyler88@gmail.com>
2025-11-02 18:27:17 -0800

    remove uneeded keys from deploying

--- provision/hosts/torus/backup.nix
+++ provision/hosts/torus/backup.nix
@@ -10,23 +10,13 @@
     owner = "${user}";
     group = "users";
   };
-  age.secrets."ssh/torus/id_ed25519.pub" = {
-    file = ../../secrets/ssh/torus/id_ed25519.pub.age;
-    owner = "${user}";
-    group = "users";
-  };
-  age.secrets."borg/torus/password" = {
-    file = ../../secrets/borg/torus/password.age;
-    owner = "${user}";
-    group = "users";
-  };
   age.secrets."borg/rsync/id_rsa" = {
     file = ../../secrets/borg/rsync/id_rsa.age;
     owner = "${user}";
     group = "users";
   };
-  age.secrets."borg/rsync/id_rsa.pub" = {
-    file = ../../secrets/borg/rsync/id_rsa.pub.age;
+  age.secrets."borg/torus/password" = {
+    file = ../../secrets/borg/torus/password.age;
     owner = "${user}";
     group = "users";
   };
