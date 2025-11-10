8429878b0eee4f027618eeedab3dd82b5360cfec
Tyler Starr <starrtyler88@gmail.com>
2025-11-10 09:36:58 -0800

    force pinentry tty

--- provision/modules/core/nixos/ssh.nix
+++ provision/modules/core/nixos/ssh.nix
@@ -1,9 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   services.openssh.enable = true;
   services.pcscd.enable = true;
+
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+    pinentryPackage = lib.mkForce pkgs.pinentry-tty;
   };
 }
