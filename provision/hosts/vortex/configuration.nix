0beac13dd49e2a023cc1962264535a93f879b1ad
Tyler Starr <starrtyler88@gmail.com>
2025-11-16 21:13:23 -0800

    static ips for vortex

--- provision/hosts/vortex/configuration.nix
+++ provision/hosts/vortex/configuration.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   user,
+  hostname,
   ...
 }:
 {
@@ -8,8 +9,36 @@
   boot.kernelPackages = pkgs.linuxPackages;
 
   # Set networking options
-  networking.firewall.enable = true;
+  networking.firewall.enable = false;
   networking.firewall.checkReversePath = false;
+
+  networking = {
+    useDHCP = false;
+    defaultGateway = "69.69.1.1";
+    nameservers = [
+      "9.9.9.9"
+      "8.8.8.8"
+      "1.1.1.1"
+    ];
+
+    interfaces = {
+      enp1s0f0.useDHCP = false;
+      enp1s0f0 = {
+        ipv4.addresses = [
+          {
+            address =
+              if "${hostname}" == "vortex-1" then
+                "69.69.1.11"
+              else if "${hostname}" == "vortex-2" then
+                "69.69.1.12"
+              else
+                "69.69.1.13";
+            prefixLength = 24;
+          }
+        ];
+      };
+    };
+  };
 
   users.users."${user}".openssh.authorizedKeys.keyFiles = [
     ../../secrets/ssh/pubs/kestrel.pub
