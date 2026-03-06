aa321e4f29f70c504c10bdbb5c52e37bc57e2f29
Tyler Starr <starrtyler88@gmail.com>
2026-03-05 20:10:11 -0800

    add kestrel kvm option

--- provision/modules/optional/desktop/scripts/display-switch.sh
+++ provision/modules/optional/desktop/scripts/display-switch.sh
@@ -29,6 +29,9 @@ case $val in
     "kestrel-living" )
         "$mutter/bin/gdctl" set "${args[@]}" --logical-monitor --primary --monitor HDMI-1 --mode 2560x1440@59.951 --scale 1.33
         ;;
+    "kestrel-kvm" )
+        "$mutter/bin/gdctl" set "${args[@]}" --logical-monitor --primary --monitor HDMI-1 --mode 1920x1080@60.000 --scale 1
+        ;;
     "kestrel-desktop" )
         "$mutter/bin/gdctl" set "${args[@]}" --logical-monitor --primary --monitor DP-2 --mode 2560x1440@143.912 --scale 1 --logical-monitor --monitor DP-1 --mode 2560x1440@143.973 --scale 1 --left-of DP-2
         ;;
