10cf08d3ea5dac584401c30f154fc6a8e235792f
Tyler Starr <starrtyler88@gmail.com>
2026-05-22 22:40:04 -0700

    enable gnome-remote-desktop

--- provision/modules/features/programs/desktop/gnome/gnome.nix
+++ provision/modules/features/programs/desktop/gnome/gnome.nix
@@ -74,6 +74,20 @@
         desktopManager.gnome.enable = true;
       };
 
+      services.gnome.gnome-remote-desktop.enable = true;
+      networking.firewall.allowedTCPPorts = [ 3389 ];
+      systemd.services.gnome-remote-desktop = {
+        wantedBy = [ "graphical.target" ];
+      };
+
+      # Disable autologin to avoid session conflicts
+      services.displayManager.autoLogin.enable = false;
+      services.getty.autologinUser = null;
+      systemd.targets.sleep.enable = false;
+      systemd.targets.suspend.enable = false;
+      systemd.targets.hibernate.enable = false;
+      systemd.targets.hybrid-sleep.enable = false;
+
       xdg.mime = {
         enable = true;
         defaultApplications = {
