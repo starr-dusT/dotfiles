88e1274a8113151b80e63cfc5edc84a1d631fc21
Tyler Starr <starrtyler88@gmail.com>
2026-07-03 11:01:55 -0700

    auto start some applications on kestrel

--- provision/modules/hosts/kestrel/programs/desktop/gnome/gnome.nix
+++ provision/modules/hosts/kestrel/programs/desktop/gnome/gnome.nix
@@ -8,6 +8,30 @@
       ];
 
       home-manager.users.${config.preferences.user} = {
+        xdg.configFile = {
+          "autostart/discord.desktop".text = ''
+            [Desktop Entry]
+            Type=Application
+            Name=Discord
+            Exec=${pkgs.discord}/bin/discord --start-minimized
+            NoDisplay=true
+          '';
+          "autostart/steam.desktop".text = ''
+            [Desktop Entry]
+            Type=Application
+            Name=Steam
+            Exec=${pkgs.steam}/bin/steam -silent
+            NoDisplay=true
+          '';
+          "autostart/mattermost.desktop".text = ''
+            [Desktop Entry]
+            Type=Application
+            Name=Discord
+            Exec=${pkgs.mattermost-desktop}/bin/mattermost-desktop
+            NoDisplay=true
+          '';
+        };
+
         dconf.settings = {
           "org/gnome/shell" = {
             enabled-extensions = [
