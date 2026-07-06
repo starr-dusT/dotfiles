7431644b3750c0fd51dbc97af43a8b24071aaefe
Tyler Starr <starrtyler88@gmail.com>
2026-07-05 18:41:19 -0700

    switch from promtail to alloy for systemd logs

--- provision/modules/features/programs/services/node-exporter.nix
+++ provision/modules/features/programs/services/node-exporter.nix
@@ -3,10 +3,7 @@
   flake.modules.nixos.node-exporter =
     { config, ... }:
     {
-      networking.firewall.allowedTCPPorts = [
-        9002
-        3031
-      ];
+      networking.firewall.allowedTCPPorts = [ 9002 ];
 
       services.prometheus = {
         exporters = {
@@ -18,40 +15,32 @@
         };
       };
 
-      services.promtail = {
-        enable = true;
-        configuration = {
-          server = {
-            http_listen_port = 3031;
-            grpc_listen_port = 0;
-          };
-          positions = {
-            filename = "/tmp/positions.yaml";
-          };
-          clients = [
-            {
-              url = "http://69.69.1.10:3030/loki/api/v1/push";
-            }
-          ];
-          scrape_configs = [
-            {
-              job_name = "journal";
-              journal = {
-                max_age = "12h";
-                labels = {
-                  job = "systemd-journal";
-                  host = "${config.preferences.hostname}";
-                };
-              };
-              relabel_configs = [
-                {
-                  source_labels = [ "__journal__systemd_unit" ];
-                  target_label = "unit";
-                }
-              ];
-            }
-          ];
-        };
-      };
+      services.alloy.enable = true;
+      environment.etc."alloy/config.alloy".text = ''
+        loki.relabel "journal" {
+          forward_to = []
+
+          rule {
+            source_labels = ["__journal__systemd_unit"]
+            target_label  = "unit"
+          }
+        }
+
+        loki.source.journal "read" {
+          max_age       = "12h"
+          relabel_rules = loki.relabel.journal.rules
+          forward_to    = [loki.write.default.receiver]
+          labels        = {
+            job  = "systemd-journal",
+            host = "${config.preferences.hostname}",
+          }
+        }
+
+        loki.write "default" {
+          endpoint {
+            url = "http://69.69.1.10:3030/loki/api/v1/push"
+          }
+        }
+      '';
     };
 }
