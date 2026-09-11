c3111c9bd12d8e7c8794aeeb3febee2f022b8bf7
Tyler Starr <starrtyler88@gmail.com>
2026-09-10 21:08:26 -0700

    backup age secrets with borg

--- provision/modules/features/programs/gaming/emulation.nix
+++ provision/modules/features/programs/gaming/emulation.nix
@@ -1,8 +1,11 @@
 { ... }:
 {
   flake.modules.nixos.emulation =
-    { ... }:
+    { pkgs, ... }:
     {
+      environment.systemPackages = with pkgs; [
+        ryubing # Experimental Nintendo Switch Emulator written in C# (community fork of Ryujinx)
+      ];
       services.flatpak.packages = [
         "net.retrodeck.retrodeck"
       ];
