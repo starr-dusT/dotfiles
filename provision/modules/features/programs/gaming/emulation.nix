37e0b221409ff398aa9ecd29ed5a3ea2accb1ebe
Tyler Starr <starrtyler88@gmail.com>
2026-09-21 18:27:19 -0700

    ignore roms and retrodeck files

--- provision/modules/features/programs/gaming/emulation.nix
+++ provision/modules/features/programs/gaming/emulation.nix
@@ -7,7 +7,7 @@
         ryubing # Experimental Nintendo Switch Emulator written in C# (community fork of Ryujinx)
       ];
       services.flatpak.packages = [
-        "net.retrodeck.retrodeck"
+        "net.retrodeck.retrodeck" # All-in-One Retro Gaming Platform for Linux
       ];
     };
 }
